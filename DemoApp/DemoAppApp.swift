458161d02d7154d7cbdbdaefd77983c20473c793
iamrohitrana <singh_rana_rohit@lilly.com>
2022-09-26 13:55:13 +0530

    First Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Rohit Singh Rana on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
